5e3af03c331777306b7d1f6b7c7fb27738a9d26b
vdiana0 <dvargas@topgroup.com.co>
2024-03-14 12:01:37 -0500

    actualizazao

---
+++ Wiki/js/main.js
@@ -0,0 +1,13 @@
+let acordeon = document.getElementsByClassName("acordeon");
+
+for (let i = 0; i < acordeon.length; i++) {
+    acordeon[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
