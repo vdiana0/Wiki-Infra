8bf88ffe6248bd391df36fb4c99d3b4a144d3771
vdiana0 <157172690+vdiana0@users.noreply.github.com>
2024-04-25 13:59:08 -0500

    actualizazao

--- Wiki/js/main.js
+++ Wiki/js/main.js
@@ -12,3 +12,17 @@ for (let i = 0; i < acordeon.length; i++) {
     });
 }
 
+
+let username = document.getElementById("usuario").value;
+let password = document.getElementById("contraseña").value;
+let btnlogin = document.getElementById("btn-btnLogin");
+
+btnlogin.addEventListener("click", function () {
+    if (username === "admin" && password === "1234") {
+        document.getElementById("contenidoPrivado").classList.remove("oculto");
+        document.getElementById("contenidoPrivado").classList.add("contenidoActivo"); 
+        document.getElementById("login").classList.add("oculto");
+    } else{
+        alert("Usuario y/o contraseña incorrectos");
+    }
+    });
